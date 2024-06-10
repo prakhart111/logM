f89906ed7242b2ca2e52d7b48755950cd4beea6e
Prakhar <ptofficial29@gmail.com>
2024-06-11 00:50:59 +0530

    @bug-fix: request format issue

--- src/ProdLogger.ts
+++ src/ProdLogger.ts
@@ -1,51 +1,53 @@
 const ProdLogger = {
   log(prodPOSTEndpoint: string, prodPOSTAuthToken: string, logLocation: string, style: string, ...content: unknown[]) {
-    postToServer('LOG', prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
+    postToServer("LOG", prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
   },
 
   error(prodPOSTEndpoint: string, prodPOSTAuthToken: string, logLocation: string, style: string, ...content: unknown[]) {
-    postToServer('ERROR', prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
+    postToServer("ERROR", prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
   },
 
   warn(prodPOSTEndpoint: string, prodPOSTAuthToken: string, logLocation: string, style: string, ...content: unknown[]) {
-    postToServer('WARN', prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
+    postToServer("WARN", prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
   },
 
   info(prodPOSTEndpoint: string, prodPOSTAuthToken: string, logLocation: string, style: string, ...content: unknown[]) {
-    postToServer('INFO', prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
+    postToServer("INFO", prodPOSTEndpoint, prodPOSTAuthToken, logLocation, ...content)
   },
 }
 
 // api call
 const postToServer = async (type: string, prodPOSTEndpoint: string, prodPOSTAuthToken: string, logLocation: string, ...content: unknown[]) => {
-  const data = {
-    type,
-    logLocation,
-    ...content,
-  }
+  const data = [
+    {
+      type,
+      logLocation,
+      ...content,
+    },
+  ]
 
   if (!isValidEndpoint(prodPOSTEndpoint)) {
-    throw new Error('Invalid endpoint')
+    throw new Error("Invalid endpoint")
   }
 
   const response = await fetch(prodPOSTEndpoint, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: prodPOSTAuthToken,
     },
     body: JSON.stringify(data),
   })
 
   if (response.ok) {
-    console.log('Log sent to server')
+    console.log("logM_beta: Log sent to server")
   } else {
-    console.error('Error sending log to server')
+    console.error("logM_beta: Error sending log to server, check the endpoint and auth token and API request format for the server")
   }
 }
 
 const isValidEndpoint = (endpoint: string) => {
-  return endpoint.startsWith('http://') || endpoint.startsWith('https://')
+  return endpoint.startsWith("http://") || endpoint.startsWith("https://")
 }
 
 export { ProdLogger }
